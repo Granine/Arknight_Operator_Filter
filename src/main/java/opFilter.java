bf185645bba5fd4b4d47b83f8c229a5d1c8b9291
guanz <guanzheng.huang@hotmail.com>
2021-11-05 13:23:38 -0700

    Initial commit

---
+++ src/main/java/opFilter.java
@@ -0,0 +1,108 @@
+
+
+import java.util.*;
+
+public class opFilter {
+    //filter by tag
+    private ArrayList<String> RcTag = new ArrayList<String>();
+    private List<String> AbTag = new ArrayList<String>();
+    //filter by status
+    private List<Integer> opRarity = new ArrayList<>();
+    private List<String> opName = new ArrayList<String>();
+    private List<String> opClass = new ArrayList<String>();
+    private List<String> opSubClass = new ArrayList<String>();
+    //filter by value
+    private List<Integer> opATK = new ArrayList<>();
+    private List<Integer> opHP = new ArrayList<>();
+    private List<Integer> opDP = new ArrayList<>();
+    private List<Integer> opRES = new ArrayList<>();
+    private List<Integer> opBLK = new ArrayList<>();
+    private List<Integer> ATINT = new ArrayList<>();
+
+    //Rep invariant
+    //RcTag, AbTag, opRarity, opName, opClass, opClass, opHp, opDp, opATK, opRES, opBLK, opATINT have >0 element
+    //RcTag, AbTag, opRarity, opName, opClass, opClass should not contain duplicate
+    //RcTag, AbTag, opName, opClass,opSubClass should only contain lowercase alphabet
+    //opRarity should only contain values in range of 1 to 6
+    //opHp, opDp, opATK, opRES, opBLK, opATINT should only contain no element or evennumber of elements and have value >1
+    //opBlk should contain value between 1 and 3
+
+    //Abstraction
+    //represents different ways to filter operators
+
+    //Safety from rep exposure
+
+    /**
+     * initialize a new full operator search
+     */
+    public opFilter (){
+        RcTag = new ArrayList<String>();
+        AbTag = new ArrayList<String>();
+        //filter by status
+        opRarity = new ArrayList<>();
+        opName = new ArrayList<String>();
+        opClass = new ArrayList<String>();
+        opSubClass = new ArrayList<String>();
+        //filter by value
+        opATK = new ArrayList<>();
+        opHP = new ArrayList<>();
+        opDP = new ArrayList<>();
+        opRES = new ArrayList<>();
+        opBLK = new ArrayList<>();
+        ATINT = new ArrayList<>();
+        System.out.println("An empty operator search has been created");
+    }
+    /**
+     * initialize a new specific field operator search
+     */
+    public opFilter (String type) {
+        switch (type.toLowerCase()) {
+            case "recruit":
+                RcTag = new ArrayList<String>();
+                System.out.println("An empty operator search of type recruitment tag has been created");
+                break;
+            case "class":
+                opClass = new ArrayList<String>();
+                opSubClass = new ArrayList<String>();
+                System.out.println("An empty operator search of type filter by classhas been created");
+                break;
+            default:
+                System.out.println("unsupported filter " + type + " try again with full filter mode");
+        }
+    }
+
+    /**
+     * return the current tag filter applied to result
+     * @return all of the current tag filter applied (Rc, Ab)
+     */
+    public Set TagFilter(){
+        HashSet currentFilter = new HashSet();
+        currentFilter.add((ArrayList)RcTag.clone());
+        currentFilter.add(AbTag);
+        ArrayList sec_list = new ArrayList();
+        sec_list = (ArrayList)RcTag.clone();
+        return currentFilter;
+    }
+
+    /**
+     * check representation invariant holds
+     * @return true if held, false if not;
+     */
+    private boolean RepCheck() {
+
+        return true;
+    }
+
+    public static void main (String[]args){
+        HashSet currentFilter = new HashSet();
+        Set currentFilterA = new HashSet();
+        currentFilter.add("A");
+        currentFilter.add("B");
+        currentFilterA.add("A");
+        currentFilterA.add("B");
+        ArrayList sec_list = new ArrayList();
+        System.out.println("??");
+
+    }
+}
+
